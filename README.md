c9303c9ee95b521cded519dc939188ef81fa425e
metbosch <metbosch@outlook.com>
2017-09-14 18:09:34 +0200

    Refactor module to use promises and periodically poll the current value

--- README.md
+++ README.md
@@ -1,35 +1,51 @@
 # homebridge-http-humidity
 
-Supports https devices on HomeBridge Platform.
-This is a simplified version of the https://github.com/lucacri/homebridge-http-temperature-humidity plugin.
-This version only supports humidity sensors, not temperature.
+Supports http/https devices on HomeBridge Platform.
+This version only supports humidity sensors returning a JSON with the data or the raw data.
+
+This plug-in acts as an interface between a web endpoint and homebridge only. You will still need some dedicated hardware to expose the web endpoints with the humidity information. In my case, I used an Arduino board with Wifi capabilities.
 
 # Installation
 
-1. Install homebridge using: npm install -g homebridge
-2. Install this plugin using: npm install -g homebridge-http-humidity
+1. Install homebridge using: `npm install -g homebridge`
+2. Install this plugin using: `npm install -g homebridge-http-humidity`
 3. Update your configuration file. See sample-config.json in this repository for a sample.
 
 # Configuration
 
+The available fields in the config.json file are:
+ - `url` [Mandatory] Endpoint URL.
+ - `name` [Mandatory] Accessory name.
+ - `http_method` [Optional] HTTP method used to get the humidity (Default: GET)
+ - `manufacturer` [Optional] Additional information for the accessory.
+ - `model` [Optional] Additional information for the accessory.
+ - `serial` [Optional] Additional information for the accessory.
+ - `field_name` [Optional] Field that will be used from the JSON response of the endpoint. Alternatively, if the `field_name` contains an empty string (`"field_name": ""`), the expected response is directly the current humidity value (Default: humidity).
+ - `timeout` [Optional] Waiting time for the endpoint response before fail (Default: 5000ms).
+ - `auth` [Optional] JSON with `user` and `pass` fields used to authenticate the request into the device.
+ - `update_interval` [Optional] If not zero, the field defines the polling period in miliseconds for the sensor state (Default is 120000ms). When the value is zero, the state is only updated when homebridge requests the current value.
 
-Configuration sample file:
+
+Example:
 
  ```
-"accessories": [
-        "accessories": [
-        {
-            "accessory": "HttpHumidity",
-            "name": "Outside Humidity",
-            "url": "http://192.168.1.210/humidity?format=json",
-            "http_method": "GET"
-        }
-    ]
+ "accessories": [
+     {
+         "accessory": "HttpHumidity",
+         "name": "Outside Humidity",
+         "url": "http://192.168.1.210/humidity?format=json",
+         "http_method": "GET",
+         "field_name": "humidity",
+         "auth": {
+             "user": "test",
+             "pass": "1234"
+         }
+     }
+ ]
 
 ```
 
-
-The defined endpoint will return a json looking like this
+The defined endpoint will return a json looking like this:
 ```
 {
 	"humidity": 38
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var Service, Characteristic;
 var request = require('request');
 
-const DEF_TIMEOUT = 5000;
+const DEF_TIMEOUT = 5000,
+      DEF_INTERVAL = 120000; //120s
 
 module.exports = function (homebridge) {
    Service = homebridge.hap.Service;
@@ -20,36 +21,81 @@ function HttpHumidity(log, config) {
    this.manufacturer = config["manufacturer"] || "@metbosch manufacturer";
    this.model = config["model"] || "Model not available";
    this.serial = config["serial"] || "Non-defined serial";
+   this.fieldName = config["field_name"] || "humidity";
    this.timeout = config["timeout"] || DEF_TIMEOUT;
+   this.auth = config["auth"];
+   this.update_interval = Number( config["update_interval"] || DEF_INTERVAL );
+
+   // Internal variables
+   this.last_value = null;
+   this.waiting_response = false;
 }
 
 HttpHumidity.prototype = {
 
-   getState: function (callback) {
-      var ops = {
-         uri:     this.url,
-         method:  this.http_method,
-         timeout: this.timeout
-      };
-      this.log('Requesting humidity on "' + ops.uri + '", method ' + ops.method);
-      request(ops, (error, res, body) => {
-         var value = null;
-         if (error) {
-            this.log('HTTP bad response (' + ops.uri + '): ' + error.message);
-         } else {
-            try {
-               value = JSON.parse(body).humidity;
-               if (value < 0 || value > 100 || isNaN(value)) {
-                  var errString = "Invalid value received (" + value + ")";
-                  throw errString;
-               }
-               this.log('HTTP successful response: ' + body);
-            } catch (parseErr) {
-               this.log('Error processing received information: ' + parseErr.message);
-               error = parseErr;
-            }
+   updateState: function () {
+      //Ensure previous call finished
+      if (this.waiting_response) {
+         this.log('Avoid updateState as previous response does not arrived yet');
+         return;
+      }
+      this.waiting_response = true;
+      this.last_value = new Promise((resolve, reject) => {
+         var ops = {
+            uri:    this.url,
+            method: this.http_method,
+            timeout: this.timeout
+         };
+         this.log('Requesting humidity on "' + ops.uri + '", method ' + ops.method);
+         if (this.auth) {
+            ops.auth = {
+               user: this.auth.user,
+               pass: this.auth.pass
+            };
          }
-         callback(error, value);
+         request(ops, (error, res, body) => {
+            var value = null;
+            if (error) {
+               this.log('HTTP bad response (' + ops.uri + '): ' + error.message);
+            } else {
+               try {
+                  value = this.fieldName === '' ? body : JSON.parse(body)[this.fieldName];
+                  value = Number(value);
+                  if (value < 0 || value > 100 || isNaN(value)) {
+                     throw new Error("Invalid value received");
+                  }
+                  this.log('HTTP successful response: ' + value);
+               } catch (parseErr) {
+                  this.log('Error processing received information: ' + parseErr.message);
+                  error = parseErr;
+               }
+            }
+            if (!error) {
+                resolve(value);
+            } else {
+                reject(error);
+            }
+            this.waiting_response = false;
+         });
+      }).then((value) => {
+         this.humidityService
+            .getCharacteristic(Characteristic.CurrentRelativeHumidity).updateValue(value, null);
+         return value;
+      }, (error) => {
+         //For now, only to avoid the NodeJS warning about uncatched rejected promises
+         return error;
+      });
+   },
+
+   getState: function (callback) {
+      this.log('Call to getState: waiting_response is "' + this.waiting_response + '"' );
+      this.updateState(); //This sets the promise in last_value
+      this.last_value.then((value) => {
+         callback(null, value);
+         return value;
+      }, (error) => {
+         callback(error, null);
+         return error;
       });
    },
 
@@ -64,6 +110,10 @@ HttpHumidity.prototype = {
       this.humidityService
          .getCharacteristic(Characteristic.CurrentRelativeHumidity)
          .on('get', this.getState.bind(this));
+
+      if (this.update_interval > 0) {
+         this.timer = setInterval(this.updateState.bind(this), this.update_interval);
+      }
 
       return [this.informationService, this.humidityService];
    }
